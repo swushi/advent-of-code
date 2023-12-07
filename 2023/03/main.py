2475ff36fb6c8d60db7b37fd3ebdebaf217a36f6
Sam Autrey <sam@pleeb.co>
2023-12-06 23:04:38 -0600

    day 3 part 2

--- 2023/03/main.py
+++ 2023/03/main.py
@@ -1,70 +1,93 @@
+import sys
+
+
 class PartNumber:
-  nums = ''
-  adjacent = False
+    nums = ''
+    adjacent_symbols: set[tuple[str, int, int]] = set()
 
-  def add_num(self, num: str):
-    self.nums += num
+    def add_num(self, num: str):
+        self.nums += num
 
-  def value(self):
-    return int(self.nums)
+    def value(self):
+        return int(self.nums)
 
-  def __str__(self):
-    return f"<PartNumber {self.nums}: {self.adjacent}>"
+    def __str__(self):
+        return f"<PartNumber {self.nums}>"
 
-  def __repr__(self):
-    return f"<PartNumber {self.nums}: {self.adjacent}>"
-
+    def __repr__(self):
+        return f"<PartNumber {self.nums}>"
 
 
 parts: list[PartNumber] = []
 
-file = open('data.txt')
+file = open(sys.argv[1])
 lines = [line.strip() for line in file.readlines()]
 
-def is_symbol(row, col):
-  try:
-    char = lines[row][col]
-    return not char.isdigit() and char != '.'
-  except:
-    return False
 
-def is_adjacent_to_symbol(row: int, col: int):
-  comparisons = [
-    (row - 1, col - 1),
-    (row - 1, col),
-    (row - 1, col + 1),
-    (row, col - 1),
-    (row, col + 1),
-    (row + 1, col - 1),
-    (row + 1, col),
-    (row + 1, col + 1)
-  ]
+def symbol_at_or_none(row, col):
+    try:
+        if row < 0 or col < 0:
+            return None
+        char = lines[row][col]
+        if char.isdigit() or char == '.':
+            return None
+        char = lines[row][col]
+        return char
+    except:
+        return None
 
-  for row, col in comparisons:
-    if is_symbol(row, col):
-      return True
 
-  return False
+def find_adjacent_symbols(row: int, col: int):
+    comparisons = [
+        (row - 1, col - 1),
+        (row - 1, col),
+        (row - 1, col + 1),
+        (row, col - 1),
+        (row, col + 1),
+        (row + 1, col - 1),
+        (row + 1, col),
+        (row + 1, col + 1)
+    ]
+
+    symbols: set[tuple[str, int, int]] = set()
+
+    for row, col in comparisons:
+        adjacent_symbol = symbol_at_or_none(row, col)
+        if adjacent_symbol is not None:
+            symbols.add((adjacent_symbol, row, col))
+
+    return symbols
+
 
 for row, line in enumerate(lines):
-  part = None 
+    part = None
 
-  for col, char in enumerate(line):
-    if char.isdigit():
-      if part is None:
-        part = PartNumber()
-        parts.append(part)
-      if part is not None:
-        part.add_num(char)
-        if not part.adjacent:
-          part.adjacent = is_adjacent_to_symbol(row, col)
-    else:
-      part = None
+    for col, char in enumerate(line):
+        if char.isdigit():
+            if part is None:
+                part = PartNumber()
+                parts.append(part)
+            if part is not None:
+                part.add_num(char)
+                part.adjacent_symbols = part.adjacent_symbols.union(
+                    find_adjacent_symbols(row, col))
+        else:
+            part = None
+
+gears_map = {}
+
+for part in parts:
+    gears = [x for x in part.adjacent_symbols if x[0] == '*']
+    for gear in gears:
+        if gear not in gears_map:
+            gears_map[gear] = list([part])
+        else:
+            gears_map[gear].append(part)
 
 sum = 0
-for part in parts:
-  if part.adjacent:
-    sum += part.value()
+for parts in gears_map.values():
+    if len(parts) == 2:
+        sum += parts[0].value() * parts[1].value()
 
 
 print(sum)
