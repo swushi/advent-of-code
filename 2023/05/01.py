5edb7ed6758aa079a1f599696d9362528eef2690
Sam Autrey <sam.autrey@discordapp.com>
2024-01-07 21:41:08 -0600

    wip

--- 2023/05/01.py
+++ 2023/05/01.py
@@ -1,32 +1,28 @@
 import sys
 import re
+from typing import List
+
+
+class CategoryMapRange:
+    def __init__(self, line: str):
+        nums = line.split(' ')
+        self.dest_start = int(nums[0])
+        self.source_start = int(nums[1])
+        self.length = int(nums[2])
+
+
+class CategoryMap:
+    def __init__(self, lines: List[str]):
+        self.source, self.destination = re.search(
+            r"(\w+)-to-(\w+) map:", lines[0]).groups()
+        self.ranges = [CategoryMapRange(line) for line in lines[1:]]
+
 
 def main():
     file = open(sys.argv[1])
     groups = list(map(lambda x: x.split('\n'), file.read().split('\n\n')))
     seeds = re.findall(r"\d+", groups[0][0])
-    seed_maps = groups[1:]
-
-    mappings = {}
-
-    for seed_map in seed_maps:
-        source, destination = re.search(r"(\w+)-to-(\w+) map:", seed_map[0]).groups()
-
-        if source not in mappings:
-            mappings[source] = {}
-
-        def mapping_fn(value):
-            
-
-
-        # mappings[source][destination] = lambda seed: seed
-
-        for mapping in seed_maps[1:]:
-            pass
-        
-
-    print(mappings)
-
+    category_maps = [CategoryMap(x) for x in groups[1:]]
 
 
 if __name__ == '__main__':
